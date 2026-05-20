f3785ca01150e91334495fbdc1de4cc18ee47e25
TynoRC <chesarobert20@gmail.com>
2026-05-20 04:39:36 +0300

    update cors production domains

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://cydevshubofficial-m3bq02tx9-tynorcs-projects.vercel.app",
+      "https://cydevshubofficial-tynorcs-projects.vercel.app",
     ],
     credentials: true,
   })
